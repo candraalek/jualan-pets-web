4f5d0264e661e09a8e8f458698c448551c493bbd
candraalek <candraalek52@gmail.com>
2025-11-13 01:00:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const pets = [
-  { name: "Titan Spider 86kg+", price: "150.000", image: "images/spider.png" },
-  { name: "Semi Godly Spider 98kg+", price: "180.000", image: "images/spider.png" },
-  { name: "Godly Spider 107kg+", price: "Sold", image: "images/spider.png" },
+  { name: "Semi HUge Spider 40kg", price: "100.000", image: "images/spider.png" },
+  { name: "Titan Spider 86.63kg", price: "150.000", image: "images/spider.png" },
+  { name: "Semi Godly Spider 98.50kg", price: "180.000", image: "images/spider.png" },
+  { name: "Godly Spider 107.63kg", price: "200.000", image: "images/spider.png" },
 ];
 
 const container = document.getElementById("pet-list");
@@ -16,6 +17,7 @@ pets.forEach(pet => {
   `;
   container.appendChild(card);
 });
+
 
 
 
