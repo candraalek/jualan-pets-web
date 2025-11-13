2e1a20467807dafea9316303af3168fc4fd1a591
candraalek <candraalek52@gmail.com>
2025-11-13 23:22:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const pets = [
   { name: "M Queen Bee base 2kg+", price: "13.000", image: "images/queen_bee.png" },
   { name: "Queen Bee base 2kg+", price: "10.000", image: "images/queen_bee.png" },
   { name: "M Butterfly ?kg+", price: "18.000", image: "images/butterfly.png" },
-  { name: "DragonFly 1kg+", price: "8.000", image: "images/dragon_fly.png" },
-  { name: "DragonFly 2kg+", price: "10.000", image: "images/dragon_fly.png" },
+  { name: "DragonFly 1kg+", price: "8.000", image: "images/dragonfly.png" },
+  { name: "DragonFly 2kg+", price: "10.000", image: "images/dragonfly.png" },
 ];
 
 const container = document.getElementById("pet-list");
@@ -32,6 +32,7 @@ pets.forEach(pet => {
   `;
   container.appendChild(card);
 });
+
 
 
 
