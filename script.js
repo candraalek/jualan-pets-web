aa83acb7585bb63568080f83bb191792853a66d8
candraalek <candraalek52@gmail.com>
2025-11-15 08:16:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const pets = [
-  { name: "NM Huge Spider 47.02kg", price: "50.000", image: "images/spider.png" },
-  { name: "NM Spider 86.63kg", price: "125.000", image: "images/spider.png" },
-  { name: "NM Spider 98.50kg", price: "150.000", image: "images/spider.png" },
-  { name: "NM Spider 107.63kg", price: "180.000", image: "images/spider.png" },
+  { name: "NM Huge Spider 47.02kg", price: "40.000", image: "images/spider.png" },
+  { name: "NM Spider 86.63kg", price: "100.000", image: "images/spider.png" },
+  { name: "NM Spider 98.50kg", price: "125.000", image: "images/spider.png" },
+  { name: "NM Spider 107.63kg", price: "150.000", image: "images/spider.png" },
   { name: "NM Echo Frog 39.26kg", price: "80.000", image: "images/echo_frog.png" },
-  { name: "NM Mimic Octopus 39,18kg", price: "80.000", image: "images/mimicoctopus.png" },
+  { name: "NM Mimic Octopus 39,18kg", price: "60.000", image: "images/mimicoctopus.png" },
   { name: "NM Mimic Octopus 26.68kg", price: "40.000", image: "images/mimicoctopus.png" },
   { name: "NM Mimic Octopus 26.33kg", price: "40.000", image: "images/mimicoctopus.png" },
-  { name: "Mimic Octopus base 1kg", price: "8.000", image: "images/mimicoctopus.png" },
-  { name: "Mimic Octopus base 2kg", price: "12.000", image: "images/mimicoctopus.png" },
-  { name: "Mimic Octopus base 2.5kg", price: "20.000", image: "images/mimicoctopus.png" },
-  { name: "Headless Horseman base 1kg+", price: "10.000", image: "images/headless_horseman.png" },
-  { name: "Headless Horseman base 2kg+", price: "15.000", image: "images/headless_horseman.png" },
+  { name: "Mimic Octopus base 1kg+(2)", price: "8.000", image: "images/mimicoctopus.png" },
+  { name: "Mimic Octopus base 2kg+(2)", price: "12.000", image: "images/mimicoctopus.png" },
+  { name: "Mimic Octopus base 2.5kg+", price: "20.000", image: "images/mimicoctopus.png" },
+  { name: "Headless Horseman base 1kg+(12)", price: "10.000", image: "images/headless_horseman.png" },
+  { name: "Headless Horseman base 2kg(5)+", price: "15.000", image: "images/headless_horseman.png" },
+  { name: "Headless Horseman base 2.5kg(3)", price: "20.000", image: "images/headless_horseman.png" },
   { name: "RB Queen Bee base 2kg+", price: "15.000", image: "images/queen_bee.png" },
   { name: "M Queen Bee base 2kg+", price: "13.000", image: "images/queen_bee.png" },
   { name: "Queen Bee base 2.09kg", price: "10.000", image: "images/queen_bee.png" },
@@ -47,6 +48,7 @@ pets.forEach(pet => {
 
   container.appendChild(card);
 });
+
 
 
 
