5a8d1c7f293afb0dac4126ce95d8073f99564e9e
candraalek <candraalek52@gmail.com>
2025-11-20 14:51:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const cards = document.querySelectorAll('.card');
+// modal
+const modal = document.getElementById("petModal");
+const modalImg = document.getElementById("modalImage");
+const modalName = document.getElementById("modalName");
+const modalFunction = document.getElementById("modalFunction");
 
-cards.forEach(card => {
-    const stokTersedia = parseInt(card.dataset.stock);
-    const inputJumlah = card.querySelector('.jumlah');
+const orderBtn = document.getElementById("orderBtn");
+const orderOptions = document.getElementById("orderOptions");
+const buyAmount = document.getElementById("buyAmount");
+const confirmOrder = document.getElementById("confirmOrder");
 
-    inputJumlah.addEventListener("input", () => {
-        let val = parseInt(inputJumlah.value);
+document.querySelectorAll(".pet-card").forEach(card => {
+    card.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = card.dataset.images;
+        modalName.innerText = card.dataset.name;
 
-        if (isNaN(val) || val < 1) {
-            inputJumlah.value = 1;
-            return;
-        }
-
-        if (val > stokTersedia) {
-            inputJumlah.value = stokTersedia;
-        }
+        modalFunction.innerHTML = 
+            "Stock: <b>" + card.dataset.stock + "</b>";
     });
 });
+
+// Close modal
+document.querySelector(".closeBtn").onclick = () => {
+    modal.style.display = "none";
+    orderOptions.style.display = "none";
+};
+
+// Open order box
+orderBtn.onclick = () => {
+    orderOptions.style.display = "block";
+};
+
+// Confirm order
+confirmOrder.onclick = () => {
+    alert("Pesanan dikonfirmasi! Jumlah: " + buyAmount.value);
+};
